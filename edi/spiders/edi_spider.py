152bea2ac1ed77da0ad777ee0bb4d2c3016c915a
sanjaysan <sanjaysan01@gmail.com>
2017-06-06 22:22:10 -0500

    Created a crawler which will crawl one data lake from a domain

---
+++ edi/spiders/edi_spider.py
@@ -0,0 +1,51 @@
+import urllib2
+
+import scrapy
+import re
+import cgi
+import os
+
+
+class EdiSpider(scrapy.Spider):
+    name = "edi"
+    url = 'https://portal.edirepository.org/nis/browseServlet?searchValue=genomics'
+    directory = url.split("=")[-1]
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    data_package = ""
+
+    def start_requests(self):
+        yield scrapy.Request(self.url, self.parse)
+
+    def parse(self, response):
+        topics_overview_page = response.css('td.nis a::attr(href)').extract_first()
+        print "response.url:", response.url
+        if topics_overview_page:
+            for data_lake_url in topics_overview_page:
+                data_lake_overview_page = response.urljoin(data_lake_url)
+                print "new_url:", data_lake_overview_page
+                EdiSpider.data_package = data_lake_overview_page.split("=")[1]
+                if not os.path.exists(EdiSpider.data_package):
+                    os.makedirs(EdiSpider.data_package)
+                yield scrapy.Request(data_lake_overview_page, callback=self.get_data_links_from_data_lake)
+
+    def get_data_links_from_data_lake(self, response):
+        print "inside get_data"
+        all_links = response.css("a.searchsubcat::attr(href)").extract()
+        regex = re.compile(".*(\\bdataviewer).*")
+        data_links = [data_link.group(0) for link in all_links for data_link in [regex.search(link)] if data_link]
+        for link in data_links:
+            yield scrapy.Request(response.urljoin(link), callback=self.download_data_from_data_lake)
+
+    def download_data_from_data_lake(self, response):
+        http_response = urllib2.urlopen(response.url)
+        _, params = cgi.parse_header(http_response.headers.get('Content-Disposition',''))
+        filename = params['filename']
+        EdiSpider.data_package = os.path.join(EdiSpider.directory + os.sep, EdiSpider.data_package)
+        with open(os.path.join(EdiSpider.data_package, filename), 'wb') as file:
+            self.logger.info("Saving file %s/%s", EdiSpider.data_package, filename)
+            file.write(response.body)
+
+from scrapy import cmdline
+cmdline.execute("scrapy crawl edi".split())
