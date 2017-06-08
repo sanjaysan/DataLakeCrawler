5665a9fe28389333206f9dea646d81b995498509
sanjaysan <sanjaysan01@gmail.com>
2017-06-08 11:42:29 -0500

    Added logic to get all the packages for a domain

--- edi/settings.py
+++ edi/settings.py
@@ -22,12 +22,20 @@ NEWSPIDER_MODULE = 'edi.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+
+# Maximum download size
+DOWNLOAD_MAXSIZE = 0
+
+# Download warn size
+DOWNLOAD_WARNSIZE = 0
+
+DOWNLOAD_DELAY = 0.25
+DOWNLOAD_TIMEOUT = 600
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- edi/spiders/edi_spider.py
+++ edi/spiders/edi_spider.py
@@ -1,9 +1,11 @@
 import urllib2
+from collections import deque
 
 import scrapy
 import re
 import cgi
 import os
+import HTMLParser
 
 
 class EdiSpider(scrapy.Spider):
@@ -14,25 +16,45 @@ class EdiSpider(scrapy.Spider):
         os.makedirs(directory)
 
     data_package = ""
+    pagination_urls = []
 
     def start_requests(self):
-        yield scrapy.Request(self.url, self.parse)
+        # self.pagination_urls.append(self.url)
+        yield scrapy.Request(self.url, callback=self.parse)
+        yield scrapy.Request(self.url, callback=self.prepare_url_list)
 
+    def prepare_url_list(self, response):
+        temp_url = response.xpath('//a[contains(@href, "simpleSearch")]').extract()
+        regex = re.compile(".*(&gt;)")
+        intermediate_url = [matcher.group(0) for url in temp_url for matcher in [regex.search(url)] if matcher]
+        if intermediate_url:
+            intermediate_url_href = re.search("simpleSearch(.*)(asc|desc)", intermediate_url[0]).group(0)
+            intermediate_url_href = HTMLParser.HTMLParser().unescape(intermediate_url_href)
+            next_page = response.urljoin(intermediate_url_href)
+            if next_page is not None:
+                # self.pagination_urls.append(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
+                yield scrapy.Request(next_page, callback=self.prepare_url_list)
+        # else:
+        #     for page in self.pagination_urls:
+        #         # print page
+        #         yield scrapy.Request(page, callback=self.parse)
+            
     def parse(self, response):
         topics_overview_page = set(response.css('td.nis a::attr(href)').extract())
         print "response.url:", response.url
         if topics_overview_page:
-            count = 1
+            # count = 1
             for data_lake_url in topics_overview_page:
                 # if count > 2:
                 #     break
                 data_lake_overview_page = response.urljoin(data_lake_url)
                 print "new_url:", data_lake_overview_page
-                EdiSpider.data_package = data_lake_overview_page.split("=")[1]
-                EdiSpider.data_package = os.path.join(EdiSpider.directory + os.sep, EdiSpider.data_package)
-                if not os.path.exists(EdiSpider.data_package):
-                    os.makedirs(EdiSpider.data_package)
-                count += 1
+                EdiSpider.package_name = data_lake_overview_page.split("=")[1]
+                EdiSpider.package_name = os.path.join(EdiSpider.directory + os.sep, EdiSpider.package_name)
+                if not os.path.exists(EdiSpider.package_name):
+                    os.makedirs(EdiSpider.package_name)
+                # count += 1
                 yield scrapy.Request(data_lake_overview_page, callback=self.get_data_links_from_data_lake)
 
     def get_data_links_from_data_lake(self, response):
@@ -47,9 +69,10 @@ class EdiSpider(scrapy.Spider):
         http_response = urllib2.urlopen(response.url)
         _, params = cgi.parse_header(http_response.headers.get('Content-Disposition',''))
         filename = params['filename']
-
-        with open(os.path.join(EdiSpider.data_package, filename), 'wb') as file:
-            self.logger.info("Saving file %s/%s", EdiSpider.data_package, filename)
+        EdiSpider.package_name = re.search("packageid=(.*)&", response.url).group(1)
+        EdiSpider.package_name = os.path.join(EdiSpider.directory + os.sep, EdiSpider.package_name)
+        with open(os.path.join(EdiSpider.package_name, filename), 'wb') as file:
+            self.logger.info("Saving file %s/%s", EdiSpider.package_name, filename)
             file.write(response.body)
 
 from scrapy import cmdline
